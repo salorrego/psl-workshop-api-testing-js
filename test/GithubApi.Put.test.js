0db5998acf773a4ebfabb08d03ffc4974cd74449
Salomon Orrego Martinez <sorregom@pslcol.com.co>
2017-09-26 16:17:25 -0500

    prueba para comprobar idempotencia del método

--- test/GithubApi.Put.test.js
+++ test/GithubApi.Put.test.js
@@ -3,7 +3,7 @@ const statusCode = require('http-status-codes');
 
 const { expect } = require('chai');
 
-describe('Given user login github', () => {
+describe.only('Given user login github', () => {
   const user = 'aperdomob';
   const urlBase = 'https://api.github.com';
 
@@ -34,6 +34,36 @@ describe('Given user login github', () => {
         expect(followedUser).to.not.equal(undefined);
         expect(followedUser.url).to.equal(`${urlBase}/users/${user}`);
         expect(followedUser.type).to.equal('User');
+      });
+    });
+  });
+  describe(`When I want to follow ${user} again`, () => {
+    let agentFollowingAgain;
+
+    before(() => {
+      agentFollowingAgain = agent.put(`${urlBase}/user/following/${user}`)
+        .auth('token', process.env.ACCESS_TOKEN);
+    });
+
+    it('Then status should be 204 and empty body Again', () =>
+      agentFollowingAgain.then((response) => {
+        expect(response.status).to.equal(statusCode.NO_CONTENT);
+        expect(response.body).to.eql({});
+      }));
+
+    describe(`When I want to check following for ${user} Again`, () => {
+      let followedUserAgain;
+
+      before(() => agent.get(`${urlBase}/user/following`)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .then((response) => {
+          followedUserAgain = response.body.find(uFollowed => uFollowed.login === 'aperdomob');
+        }));
+
+      it(`Then I should get following ${user} Again`, () => {
+        expect(followedUserAgain).to.not.equal(undefined);
+        expect(followedUserAgain.url).to.equal(`${urlBase}/users/${user}`);
+        expect(followedUserAgain.type).to.equal('User');
       });
     });
   });
