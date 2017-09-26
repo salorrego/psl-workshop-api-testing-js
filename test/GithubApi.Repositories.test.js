0998f31d92440e3bd7dc8cf3303dfcd29c92b52f
Salomon Orrego Martinez <sorregom@pslcol.com.co>
2017-09-26 10:41:55 -0500

    prueba de descarga del zip exitoso

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -1,6 +1,6 @@
 const agent = require('superagent-promise')(require('superagent'), Promise);
 const chai = require('chai');
-// const md5 = require('md5');
+const md5 = require('md5');
 
 // chai.use(require('chai-subset'));
 
@@ -21,7 +21,7 @@ describe('Given a user is logged in GitHub', () => {
           userInfo = response.body;
         }));
 
-    it(`Then ${userName} info should be returned`, () => {
+    xit(`Then ${userName} info should be returned`, () => {
       expect(userInfo.name).to.equal('Alejandro Perdomo');
       expect(userInfo.company).to.equal('PSL');
       expect(userInfo.location).to.equal('Colombia');
@@ -38,13 +38,30 @@ describe('Given a user is logged in GitHub', () => {
             repository = response.body.find(repos => repos.name === expectedRepository);
           }));
 
-      it(`Then ${expectedRepository} should exist`, () => {
+      xit(`Then ${expectedRepository} should exist`, () => {
         assert.exists(repository);
         expect(repository.full_name).to.equal('aperdomob/jasmine-awesome-report');
         expect(repository.private).to.equal(false);
         expect(repository.language).to.equal('JavaScript');
         expect(repository.default_branch).to.equal('development');
         expect(repository.description).to.equal('An awesome html report for Jasmine');
+      });
+
+      describe(`When ${expectedRepository} main branch is downloaded`, () => {
+        const numExpectedMd5 = '81b4fd5281eff113c603bcc8f6f6cc4f';
+        let zip;
+
+        before(() =>
+          agent.get(`${repository.svn_url}/archive/${repository.default_branch}.zip`)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .buffer(true)
+            .then((response) => {
+              zip = response.text;
+            }));
+
+        it('Then the zip archive should be downloaded correctly', () => {
+          expect(md5(zip)).to.equal(numExpectedMd5);
+        });
       });
     });
   });
