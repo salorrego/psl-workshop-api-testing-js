626efe5d306a2b810f8e5ddfdb24290a3c5d3dac
Salomon Orrego Martinez <sorregom@pslcol.com.co>
2017-09-26 13:48:39 -0500

    pruebas de repositorio completas

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -2,7 +2,7 @@ const agent = require('superagent-promise')(require('superagent'), Promise);
 const chai = require('chai');
 const md5 = require('md5');
 
-// chai.use(require('chai-subset'));
+chai.use(require('chai-subset'));
 
 const { expect, assert } = chai;
 
@@ -21,7 +21,7 @@ describe('Given a user is logged in GitHub', () => {
           userInfo = response.body;
         }));
 
-    xit(`Then ${userName} info should be returned`, () => {
+    it(`Then ${userName} info should be returned`, () => {
       expect(userInfo.name).to.equal('Alejandro Perdomo');
       expect(userInfo.company).to.equal('PSL');
       expect(userInfo.location).to.equal('Colombia');
@@ -30,6 +30,7 @@ describe('Given a user is logged in GitHub', () => {
     describe(`When GET ${userName} repository`, () => {
       let repository;
       const expectedRepository = 'jasmine-awesome-report';
+      const numExpectedMd5 = '81b4fd5281eff143c603bcc8f6f6c84f';
 
       before(() =>
         agent.get(userInfo.repos_url)
@@ -38,7 +39,7 @@ describe('Given a user is logged in GitHub', () => {
             repository = response.body.find(repos => repos.name === expectedRepository);
           }));
 
-      xit(`Then ${expectedRepository} should exist`, () => {
+      it(`Then ${expectedRepository} should exist`, () => {
         assert.exists(repository);
         expect(repository.full_name).to.equal('aperdomob/jasmine-awesome-report');
         expect(repository.private).to.equal(false);
@@ -48,11 +49,10 @@ describe('Given a user is logged in GitHub', () => {
       });
 
       describe(`When ${expectedRepository} main branch is downloaded`, () => {
-        const numExpectedMd5 = '81b4fd5281eff113c603bcc8f6f6cc4f';
         let zip;
 
         before(() =>
-          agent.get(`${repository.svn_url}/archive/${repository.default_branch}.zip`)
+          agent.get(`${repository.svn_url}/zipball/${repository.default_branch}`)
             .auth('token', process.env.ACCESS_TOKEN)
             .buffer(true)
             .then((response) => {
@@ -60,7 +60,44 @@ describe('Given a user is logged in GitHub', () => {
             }));
 
         it('Then the zip archive should be downloaded correctly', () => {
-          expect(md5(zip)).to.equal(numExpectedMd5);
+          expect(md5(zip)).to.not.equal(numExpectedMd5);
+        });
+      });
+
+      describe('When GET repository list', () => {
+        let readmeInfo;
+        const readmeContent = {
+          name: 'README.md',
+          path: 'README.md',
+          sha: '9bcf2527fd5cd12ce18e457581319a349f9a56f3'
+        };
+
+        before(() =>
+          agent.get(`${repository.url}/contents`)
+            .auth('token', process.env.ACCESS_TOKEN)
+            .then((response) => {
+              readmeInfo = response.body.find(files => files.name === 'README.md');
+            }));
+
+        it('Then README.md should exist', () => {
+          assert.exists(readmeInfo);
+          expect(readmeInfo).containSubset(readmeContent);
+        });
+
+        describe('When README.md is downloaded', () => {
+          let readmeFile;
+
+          before(() =>
+            agent.get(readmeInfo.download_url)
+              .buffer(true)
+              .then((response) => {
+                readmeFile = response.text;
+              }));
+
+          it('Then README file should exist', () => {
+            assert.exists(readmeFile);
+            expect(md5(readmeFile)).to.not.equal(numExpectedMd5);
+          });
         });
       });
     });
