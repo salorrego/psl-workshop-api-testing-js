94b69965893f595669ab43b8b01146caf20acb4c
Salomon Orrego <salorrego@gmail.com>
2017-09-26 21:31:11 -0500

    prueba de minimo un repositorio

---
+++ test/GithubApi.Issue.test.js
@@ -0,0 +1,22 @@
+const agent = require('superagent-promise')(require('superagent', Promise));
+
+const { expect } = require('chai');
+
+describe('Given user loged in github', () => {
+  const user = 'salorrego';
+  const urlBase = 'https://api.github.com';
+
+  describe(`When I look for the user ${user}`, () => {
+    let userInfo;
+
+    before(() => agent.get(`${urlBase}/user`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .then((response) => {
+        userInfo = response.body;
+      }));
+
+    it.only('Then the user should have at least one repository', () => {
+      expect(userInfo.public_repos).to.be.greaterThan(0);
+    });
+  });
+});
